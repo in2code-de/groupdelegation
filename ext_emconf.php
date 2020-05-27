e9b45decce6f2c5ec42637e5df51768dbad8668e
Andreas Sommer <a.sommer@tum.de>
2020-05-27 11:47:45 +0200

    Add enableMultiSelectFilterTextfield to be_groups.delegateable and set php version compatibility up to 7.2.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,8 +4,8 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'groupdelegation',
     'description' => 'Allows generating Sub-Admin be_user by groups which can handle access of other be_user by groups',
     'category' => 'be',
-    'author' => 'Sebastian Mueller, Bernhard Maier, Franz Kugelmann, Marcus Schwemer',
-    'author_email' => 'sebastian.mueller@abezet.de, maier@tum.de, franz.kugelmann@elementare-teilchen.de, marcus@schwemer.de',
+    'author' => 'Sebastian Mueller, Bernhard Maier, Franz Kugelmann, Marcus Schwemer, Andreas Sommer',
+    'author_email' => 'sebastian.mueller@abezet.de, maier@tum.de, franz.kugelmann@elementare-teilchen.de, marcus@schwemer.de, a.sommer@tum.de',
     'author_company' => 'A.BE.ZET GmbH, TU München, Elementare Teilchen GmbH, in2code GmbH',
     'conflicts' => '',
     'priority' => '',
@@ -17,11 +17,11 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '3.1.0',
+    'version' => '3.1.1',
     'constraints' => [
         'depends' => [
             'typo3' => '8.4.0 - 8.99.99',
-            'php' => '7.0.0-7.1.99'
+            'php' => '7.0.0-7.2.99'
         ],
         'conflicts' => [
         ],
